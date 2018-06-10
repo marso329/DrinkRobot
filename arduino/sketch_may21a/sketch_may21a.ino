3a1f427594054f69c1f5797d587a53e793cd4e6f
Martin Söderén <marso329@student.liu.se>
2018-06-10 21:59:19 +0200

    better pwm code

--- arduino/sketch_may21a/sketch_may21a.ino
+++ arduino/sketch_may21a/sketch_may21a.ino
@@ -35,10 +35,30 @@ digitalWrite(32, HIGH);
 digitalWrite(33, HIGH);
 
 //pwm
-pinMode(2, OUTPUT); 
-pwm25kHzBegin();
-pwmDuty(39);
+//pinMode(2, OUTPUT); 
+//pwm25kHzBegin();
+//pwmDuty(39);
 
+   TCCR4A = 0;
+    TCCR4B = 0;
+    TCNT4  = 0;
+
+    // Mode 10: phase correct PWM with ICR4 as Top (= F_CPU/2/25000)
+    // OC4C as Non-Inverted PWM output
+    ICR4   = (F_CPU/25000)/2;
+    OCR4C  = ICR4/2;                    // default: about 50:50
+    TCCR4A = _BV(COM4C1) | _BV(WGM41);
+    TCCR4B = _BV(WGM43) | _BV(CS40);
+
+
+    // Set the PWM pin as output.
+    pinMode( 2, OUTPUT);
+    analogWrite25k(39);
+
+}
+void analogWrite25k(int value)
+{
+    OCR4C = value;
 }
 
 void pwm25kHzBegin() {
