3302f2ef448dc44d8ea07eac4e9c3e2edc1bbbb4
Martin Söderén <marso329@student.liu.se>
2018-06-10 21:34:33 +0200

    better pwm code

--- arduino/sketch_may21a/sketch_may21a.ino
+++ arduino/sketch_may21a/sketch_may21a.ino
@@ -36,8 +36,23 @@ digitalWrite(33, HIGH);
 
 //pwm
 pinMode(2, OUTPUT); 
-analogWrite(2, 127);
+pwm25kHzBegin();
+pwmDuty(39);
 
+}
+
+void pwm25kHzBegin() {
+  TCCR2A = 0;                               // TC2 Control Register A
+  TCCR2B = 0;                               // TC2 Control Register B
+  TIMSK2 = 0;                               // TC2 Interrupt Mask Register
+  TIFR2 = 0;                                // TC2 Interrupt Flag Register
+  TCCR2A |= (1 << COM2B1) | (1 << WGM21) | (1 << WGM20);  // OC2B cleared/set on match when up/down counting, fast PWM
+  TCCR2B |= (1 << WGM22) | (1 << CS21);     // prescaler 8
+  OCR2A = 79;                               // TOP overflow value (Hz)
+  OCR2B = 0;
+}
+void pwmDuty(byte ocrb) {
+  OCR2B = ocrb;                             // PWM Width (duty)
 }
 
 // the loop routine runs over and over again forever:
